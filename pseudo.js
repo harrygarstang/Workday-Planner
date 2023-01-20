d8cf2c0b95139b626df8e3ba0075eccaac8a188c
harryg <harry@gmail.com>
2023-01-20 20:02:34 +0000

    Added initial JS which implements the current time using Moment.js and a work in progress function that will dynamically generate a work day planner based on the user-inputted working hours

--- pseudo.js
+++ pseudo.js
@@ -2,3 +2,26 @@
 var today = moment();
 $("#currentDay").text(today.format("Do MMMM YYYY")); 
 
+var plannerContainer = $('#planner');
+
+ function generateWorkdayPlanner(startHour,endhour){
+    for(i=startHour; i<endhour;i++);
+
+    // Dynamically create time-block
+    var timeBlock = $("<div>");
+    timeBlock.attr('class','row-time-block');
+
+    // Create a column to display the time
+    var timeColumn = $("<div>");
+    timeColumn.attr('class', 'col-lg-1 col-md-1 hour');
+
+    // Create an area for the text
+    var txtArea = $('<textarea>');
+
+    // Create the button for saving 
+
+    // Append the dynamically created timeblocks 
+
+ }
+
+ generateWorkdayPlanner(9,17);
